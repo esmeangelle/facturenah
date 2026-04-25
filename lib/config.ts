e4945a57a6e6e3553ba66cafc716d18baab33ca3
tianaflorent <tianaflorent75@gmail.com>
2026-04-25 17:56:44 +0000

    mdf

--- lib/config.ts
+++ lib/config.ts
@@ -6,16 +6,16 @@
 
 // export default API_URL;
 
-// const API_URL = process.env.NEXT_PUBLIC_API_URL || 
-//   (typeof window !== 'undefined' 
-//     ? `http://${window.location.hostname}:5000`
-//     : 'http://localhost:5000');
-
-// export default API_URL;
-
-const API_URL = process.env.NEXT_PUBLIC_API_URL 
-  ?? (typeof window !== 'undefined' 
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 
+  (typeof window !== 'undefined' 
     ? `http://${window.location.hostname}:5000`
     : 'http://localhost:5000');
 
 export default API_URL;
+
+// const API_URL = process.env.NEXT_PUBLIC_API_URL 
+//   ?? (typeof window !== 'undefined' 
+//     ? `http://${window.location.hostname}:5000`
+//     : 'http://localhost:5000');
+
+// export default API_URL;
