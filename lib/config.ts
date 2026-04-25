a6768ee36aaac6c73df0499e200840b952e128c8
tianaflorent <tianaflorent75@gmail.com>
2026-04-25 16:04:08 +0000

    modif

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,13 @@
-// config/dg.js
+// // config/dg.js
 
-const API_URL = typeof window !== 'undefined'
-  ? `http://${window.location.hostname}:5000`
-  : 'http://localhost:5000';
+// const API_URL = typeof window !== 'undefined'
+//   ? `http://${window.location.hostname}:5000`
+//   : 'http://localhost:5000';
+
+// export default API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 
+  (typeof window !== 'undefined' 
+    ? `http://${window.location.hostname}:5000`
+    : 'http://localhost:5000');
 
 export default API_URL;
